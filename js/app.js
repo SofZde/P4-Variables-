7de07d3697a6d95e5050022b79c80825a2599a2a
SofZde <sofzde138@ihmeducation.se>
2024-09-12 12:57:51 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 let numberOfBottles= 10;
 const Line1= "green bottles hanging on the wall ";
 const Line2= "and if one bottle, should accidentally fall, there will be "
-let Line3 =9;
 
-console.log(numberOfBottles, Line1, Line2,Line3 )
+
+console.log(numberOfBottles);
+
+
+console.log(Line1)
+console.log(Line2)
+console.log(--numberOfBottles);
+
+
 
 
 
